a854c822bc1e19a1e3bff02d820c2eadf1873d9e
Silvana-Ruiz <silvanadorita@hotmail.com>
2024-02-28 15:01:49 -0700

    add navigation for form and history screens

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from './components/Tabs';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Tabs />
+    </NavigationContainer>
+
   );
 }
 
---
+++ screens/Registro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const Registro = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Registro Screen</Text>
+      </View>
+    </SafeAreaView>
+
+
+  );
+};
+
+export default Registro;
