44dcef923c0651deb5ec845c28804574101be06b
shininstef <phanye2002@gmail.com>
2024-03-03 16:18:22 -0700

    add buttons w/o style

---
+++ components/CustomBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TouchableOpacity, Text, View, StyleSheet, Button} from 'react-native';
+
+const CustomBtn = (props) =>  {
+    return (
+        <>
+            <View>
+                <Button title='Mejores Donadores' />
+            </View>
+        </>
+    )
+}
--- screens/Historial.js
+++ screens/Historial.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 const Historial = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Historial Screen</Text>
+      <View style={viewStyle.content}>
+        <SearchBar
+          placeholder="Buscar..." 
+          inputStyle = {{backgroundColor: 'white'}}
+          containerStyle={{backgroundColor: 'white', borderWidth: 1, borderRadius: 5}}
+          inputContainerStyle={{backgroundColor: 'white'}}
+          />
+
+        
+        <Button title='Fecha' color="#ff5c5c" style={viewStyle.buttonStyle}/>
+        <Button title='Mejores Donadores' color="#ff5c5c" style={viewStyle.buttonStyle}/>
+        <Button title='Peores Donadores' color="#ff5c5c" style={viewStyle.buttonStyle}/>
+        <Button title='Carga Ciega' color="#ff5c5c" style={viewStyle.buttonStyle}/>
       </View>
     </SafeAreaView>
   );
 };
+
+const viewStyle = StyleSheet.create({
+  content: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonStyle:{
+    borderRadius:'10',
+    padding: 'auto',
+    height: 50,
+    width:200
+  }
+})
 
 export default Historial;
